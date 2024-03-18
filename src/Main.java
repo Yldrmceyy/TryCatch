ecdfd67cda73abd87ddce8c85aeecaa9a1972492
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-03-18 10:18:07 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Defining variables
+        int[] arr = {3,8,2,5,4,1,6,7,9,0};
+        int index;
+        Scanner input = new Scanner(System.in);
+
+        //System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Enter the index number to get the desired element in the array:");
+        index = input.nextInt();
+
+        // Call indexOfArr method
+        indexOfArr(arr,index);
+    }
+
+    // indexOfArr method
+    static public void indexOfArr(int[] arr, int index){
+        try{
+            //Block of code where an exception is predicted to occur
+            System.out.println(arr[index]);
+        }catch(ArrayIndexOutOfBoundsException e){
+            // Throw error message
+            System.out.println(e.getMessage());
+        }
     }
 }
